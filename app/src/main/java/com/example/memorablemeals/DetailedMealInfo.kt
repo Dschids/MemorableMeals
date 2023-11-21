d95d9a55907cab89291165a5185699da930402c5
Dschids <123191756+Dschids@users.noreply.github.com>
2023-11-21 14:27:49 -0600

    last 2 layouts

---
+++ app/src/main/java/com/example/memorablemeals/DetailedMealInfo.kt
@@ -0,0 +1,11 @@
+package com.example.memorablemeals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedMealInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_meal_info)
+    }
+}
---
+++ app/src/main/java/com/example/memorablemeals/EditMealInfo.kt
@@ -0,0 +1,11 @@
+package com.example.memorablemeals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditMealInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_meal_info)
+    }
+}
